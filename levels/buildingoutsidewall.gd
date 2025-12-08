edb13a97814eee30e6c12dcb503c7297c557337a
Jeremy Shields <Jer@POWERHOUSEPOINT.COM>
2025-12-08 11:28:27 -0700

    aaa

--- levels/buildingoutsidewall.gd
+++ levels/buildingoutsidewall.gd
@@ -1,12 +1,24 @@
 extends Area2D
-@onready var playertrigger: Area2D = $"../../playertrigger"
+
+@onready var player := get_tree().get_nodes_in_group("player")[0]
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	visible = true
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		visible = true
+
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		visible = false
