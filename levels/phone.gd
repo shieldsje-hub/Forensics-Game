3d925cc24945bb4b3108756af800571ada179925
Jeremy Shields <Jer@POWERHOUSEPOINT.COM>
2026-01-08 09:38:47 -0700

    phone on wall now

---
+++ levels/phone.gd
@@ -0,0 +1,14 @@
+extends Node2D
+var in_range := false
+
+func _process(_delta):
+	if in_range and Input.is_action_just_pressed("pickup"):
+		get_tree().change_scene_to_file("res://phone.tscn")
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		in_range = true
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		in_range = false
